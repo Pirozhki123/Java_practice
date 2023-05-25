8d6032df2fc097b8185e582238accc92d29ec316
Pirozhki <20508755+Pirozhki123@users.noreply.github.com>
2023-05-25 06:56:59 +0000

    1章完了

---
+++ 01/P1_2.java
@@ -0,0 +1,7 @@
+public class P1_2 {
+  public static void main (String[] args) {
+    int a = 3;
+    int b = 5;
+    System.out.println("立幅" + a + "横幅" + b + "の長方形の面積は、" + a * b);
+  }
+}
---
+++ 01/P1_3.java
@@ -0,0 +1,11 @@
+public class P1_3 {
+  public static void main (String[] args) {
+    boolean a = true;
+    // String b = "駆";
+    char b = "駆";
+    double c = 3.14;
+    // String d = "314159265853979L";
+    long d = 314159265853979L;
+    String e = "ミナトの攻撃！敵に15ポイントのダメージを与えた。"
+  }
+}
