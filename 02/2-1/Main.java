fe25f17399695df74f66fe764c75d300aef77dcc
Pirozhki <20508755+Pirozhki123@users.noreply.github.com>
2023-05-26 06:40:53 +0000

    2.2まで

---
+++ 02/2-1/Main.java
@@ -0,0 +1,10 @@
+public class main {
+  public static void main(String[] args) {
+    int a;
+    int b;
+    a = 20;
+    b = a + 5;
+    System.out.println(a);
+    System.out.println(b);
+  }
+}
---
+++ 02/2-2/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+  public static void main(String[] args) {
+    // エラーで実行できない例
+    System.out.println("私の好きな記号は二重引用符(")です");
+  }
+}
