d36d98d3f18b992a0e89649c68fcbc00217ce06c
Pirozhki <20508755+Pirozhki123@users.noreply.github.com>
2023-05-27 07:26:40 +0000

    2.5まで

---
+++ 02/2-8/Main.java
@@ -0,0 +1,9 @@
+// 異なる方同士の算術演算
+public class Main {
+  public static void main(String[] args){
+    double d = 8.5 / 2;
+    long l = 5 + 2L;
+    System.out.println(d);
+    System.out.println(l);
+  }
+}
