48e345a75662e0176803922fdae77f24097475b6
Pirozhki <20508755+Pirozhki123@users.noreply.github.com>
2023-05-28 05:55:43 +0000

    3.1まで

---
+++ 03/3-1/Main.java
@@ -0,0 +1,12 @@
+// 天気による行動の変化をJavaで表す
+public class Main {
+  public static void main(String[] args) {
+    boolean tenki = false;
+    if (tenki == true) {
+      System.out.println("洗濯をします");
+      System.out.println("散歩に行きます");
+    } else {
+      System.out.println("DVDを見ます");
+    }
+  }
+}
---
+++ 03/3-2/Main.java
@@ -0,0 +1,10 @@
+// トイレの空をまつ繰り返し処理
+public class Main {
+  public static void main(String[] args) {
+    boolean doorClose = true;
+    while (doorClose == true) {
+      System.out.println("ノックする");
+      System.out.println("1分待つ");
+    }
+  }
+}
