f52a084c143219cfde117bfcbeae21cf40057431
Pirozhki <20508755+Pirozhki123@users.noreply.github.com>
2023-05-30 04:47:43 +0000

    3章まで

---
+++ 03/3-9/Main.java
@@ -0,0 +1,12 @@
+// for文のループを二重にして九九の表を出力
+public class Main {
+  public static void main(String[] args) {
+    for (int i = 1; i < 10; i++) {
+      for(int j = 1; j < 10; j++) {
+        System.out.print(i * j);
+        System.out.print("");
+      }
+      System.out.println("");
+    }
+  }
+}
---
+++ 03/p3-3/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+  public static void main(String[] args) {
+    int isHungry = 1;
+    String food = "おにぎり";
+    System.out.println("こんにちは");
+    if(isHungry == 0) {
+      System.out.println("お腹がいっぱいです");
+    } else {
+      System.out.println("はらぺこです");
+      // System.out.println(food + "をいただきます");
+      // System.out.println("ごちそうさまでした");
+    }
+    if (isHungry == 1) {
+      System.out.println(food + "をいただきます");
+    }
+    System.out.println("ごちそうさまでした");
+  }
+}
