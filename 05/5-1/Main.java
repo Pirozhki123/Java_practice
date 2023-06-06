29176a90d4770b76d993fcbcc8f8e4f64111d4be
Pirozhki <20508755+Pirozhki123@users.noreply.github.com>
2023-06-06 05:46:18 +0000

    5.2まで

---
+++ 05/5-1/Main.java
@@ -0,0 +1,8 @@
+// シンプルなメソッドの定義(メインメソッド未定義の為エラーが出る)
+public class Main{
+  // helloメソッドの定義
+  public static void hello() {
+    // helloメソッドが呼び出されたときの処理
+    System.out.println("湊さん、こんにちは");
+  }
+}
