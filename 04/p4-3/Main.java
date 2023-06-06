feeb205412c784a7c0d8b19c1336fc07f2984359
Pirozhki <20508755+Pirozhki123@users.noreply.github.com>
2023-06-06 04:56:02 +0000

    4章終了

---
+++ 04/p4-3/Main.java
@@ -0,0 +1,12 @@
+// 練習4-3
+public class Main {
+  public static void main(String[] args) {
+    int[] counts = null;
+    float[] heights = {171.3F, 175.0F};
+    // NullPointerException
+    // System.out.println(counts[1]);
+
+    // ArrayIndexOutOfBoundsException
+    // System.out.println(heights[2]);
+  }
+}
---
+++ 04/p4-4/Main.java
@@ -0,0 +1,13 @@
+// 練習4-4
+public class Main {
+  public static void main(String[] args) {
+    int[] numbers = {3, 4, 9};
+    System.out.println("1桁の数字を入力してください");
+    int input = new java.util.Scanner(System.in).nextInt();
+    for (int i = 0; i < numbers.length; i++) {
+      if (numbers[i] == input) {
+        System.out.println("アタリ！");
+      }
+    }
+  }
+}
