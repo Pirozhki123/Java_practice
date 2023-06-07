08fc20182882094401bdbd0ae4708d03f00c73a8
Pirozhki <20508755+Pirozhki123@users.noreply.github.com>
2023-06-07 09:28:36 +0000

    5.2.3まで

---
+++ 05/5-4/Main.java
@@ -0,0 +1,13 @@
+// 引数の例（渡す値が１つの場合）
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("メソッドを呼び出します");
+    hello("湊");
+    hello("朝香");
+    hello("菅原");
+    System.out.println("メソッドの呼び出しが終わりました");
+  }
+  public static void hello(String name) {
+    System.out.println(name + "さん、こんにちは");
+  }
+}
