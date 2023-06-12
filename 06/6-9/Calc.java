7c0e97d5b441d40000b63fb461f0762315705b76
Pirozhki <20508755+Pirozhki123@users.noreply.github.com>
2023-06-12 04:09:48 +0000

    6.3まで

---
+++ 06/6-9/Calc.java
@@ -0,0 +1,12 @@
+// calcapp.logicsの全クラスをインポートする
+package calcapp.main;
+import calcapp.logics.*;
+
+public class Calc {
+  public static void main(String[] args) {
+    int a = 10; int b = 2;
+    int total = CalcLogic.tasu(a, b);
+    int delta = calcapp.logics.CalcLogic.hiku(a, b);
+    System.out.println("足すと" + total + "、引くと" + delta);
+  }
+}
