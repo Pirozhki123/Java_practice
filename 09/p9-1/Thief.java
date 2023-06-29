f94e4e63b3fbf2405c7e8b8bb6fad03b8b0fc320
Pirozhki <20508755+Pirozhki123@users.noreply.github.com>
2023-06-29 02:16:14 +0000

    9章完了

---
+++ 09/p9-1/Thief.java
@@ -0,0 +1,18 @@
+// 練習9-1
+public class Thief {
+  String name;
+  int hp;
+  int mp;
+  public Thief(String name, int HP, int MP) {
+    this.name = name;
+    this.hp = HP;
+    this.mp = MP;
+  }
+  public Thief(String name, int HP) {
+    this(name, HP, 5);
+  }
+  public Thief(String name) {
+    // this(name, 40, 5);
+    this(name, 40);
+  }
+}
